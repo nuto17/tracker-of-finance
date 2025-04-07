b0a0c876d3b0669f3deb94bb9606536148042c75
nuto17 <nutolfg@gmail.com>
2025-04-07 20:50:29 +0300

    wallet dto

--- src/main/java/com/example/financeTracker/dto/WalletDto.java
+++ src/main/java/com/example/financeTracker/dto/WalletDto.java
@@ -1,9 +1,14 @@
 package com.example.financeTracker.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Data;
+
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class WalletDto {
 
     private Long id;
@@ -11,4 +16,7 @@ public class WalletDto {
     private String name;
 
     private BigDecimal balance;
+
+    @Schema(hidden = true)
+    private LocalDateTime updateTime;
 }
