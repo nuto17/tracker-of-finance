bde965ccde17b7852c87a729a4d9eebf6e824814
nuto17 <nutolfg@gmail.com>
2025-04-07 20:51:27 +0300

    fix mapper

--- src/main/java/com/example/financeTracker/mapper/WalletMapper.java
+++ src/main/java/com/example/financeTracker/mapper/WalletMapper.java
@@ -1,24 +1,17 @@
 package com.example.financeTracker.mapper;
 
-
 import com.example.financeTracker.dto.WalletDto;
 import com.example.financeTracker.model.Wallet;
-import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Named;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface WalletMapper {
 
-    @Named("fullMappingForCreate")
     Wallet toModel(WalletDto walletDto);
-
     WalletDto toDto(Wallet wallet);
 
-    @IterableMapping(qualifiedByName = "fullMappingForCreate")
     List<Wallet> toModel(List<WalletDto> walletsDto);
-
     List<WalletDto> toDto(List<Wallet> wallets);
 }
