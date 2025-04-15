81e6b677c6917c7104660aabf56ccfc648b94e77
nuto17 <nutolfg@gmail.com>
2025-04-15 11:33:12 +0300

    crud expense

--- src/main/java/com/example/financetracker/service/ExpenseService.java
+++ src/main/java/com/example/financetracker/service/ExpenseService.java
@@ -13,8 +13,6 @@ import java.util.List;
 public class ExpenseService {
 
     private final ExpenseRepository expenseRepository;
-    private final WalletService walletService;
-    private final CategoryService categoryService;
 
     public List<Expense> getExpenses() {
         return expenseRepository.findAll();
@@ -26,36 +24,6 @@ public class ExpenseService {
     }
 
     public Expense createExpense(Expense expense) {
-        walletService.makeExpense(expense);
-        categoryService.makeExpense(expense);
         return expenseRepository.save(expense);
     }
-
-    public void deleteExpenseById(Long id) {
-        expenseRepository.deleteById(id);
-    }
 }
-//    public Expense updateExpense(Expense expense, Long id) {
-//        Long oldWalletId = getExpenseById(id).getWallet().getId();
-//        BigDecimal oldAmount = getExpenseById(id).getAmount();
-//        BigDecimal newAmount = expense.getAmount();
-//        Category oldCategory = getExpenseById(id).getCategory();
-//        Long newCategoryId = expense.getCategory().getId();
-//
-//        Wallet updatedWallet = walletService.updateWalletFromExpense(oldWalletId, expense.getWallet().getId(), oldAmount, newAmount);
-//        return Expense.builder()
-//                .id(id)
-//                .category(expense.getCategory())
-//                .timeAdded(expense.getTimeAdded())
-//                .wallet(updatedWallet)
-//                .build();
-//    }
-//дописать логику( нужно чтобы при обновлении траты -> шел перерасчет всего: кошелек, баланс категории
-
-// пока не воркает
-
-
-//        if(newExpense.getWallet().getId().compareTo(walletService.getWalletById(oldWalletId).getId())!=0 && isNotAmountChanged){
-//            walletService.addMoneyToWalletById(oldAmount,oldWalletId);
-//            walletService.substractMoneyFromWalletById(oldAmount,newExpense.getWallet().getId());
-//        }
