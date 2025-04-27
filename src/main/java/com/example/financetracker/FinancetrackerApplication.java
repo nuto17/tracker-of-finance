636138e42f92a0d161b1734d0efd713a1b379965
nuto17 <nutolfg@gmail.com>
2025-04-27 22:31:32 +0300

    some fix

--- src/main/java/com/example/financetracker/FinancetrackerApplication.java
+++ src/main/java/com/example/financetracker/FinancetrackerApplication.java
@@ -1,11 +1,14 @@
 package com.example.financetracker;
 
+import com.example.financetracker.config.propert.UrlBasedCorsConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(UrlBasedCorsConfigurationProperties.class)
 public class FinancetrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinancetrackerApplication.class, args);
