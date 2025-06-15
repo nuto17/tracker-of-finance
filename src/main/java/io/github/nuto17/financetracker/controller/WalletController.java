ac033f5fe2f2a4389a907128895fa3e1f3fa7a76
nuto17 <nutolfg@gmail.com>
2025-06-15 17:31:30 +0300

    Feature/check id validated (#33)
    * method for sum transactions by categoryId
    
    * validated walletId, categoryId

--- src/main/java/io/github/nuto17/financetracker/controller/WalletController.java
+++ src/main/java/io/github/nuto17/financetracker/controller/WalletController.java
@@ -8,13 +8,16 @@ import io.github.nuto17.financetracker.marks.TransactionType;
 import io.github.nuto17.financetracker.model.Transaction;
 import io.github.nuto17.financetracker.model.Wallet;
 import io.github.nuto17.financetracker.service.WalletService;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@Validated
 @RequiredArgsConstructor
 @RequestMapping(path = "api/wallets")
 public class WalletController {
@@ -30,7 +33,7 @@ public class WalletController {
     }
 
     @GetMapping("/{id}")
-    public WalletDto getWalletById(@PathVariable("id") Long id) {
+    public WalletDto getWalletById(@PathVariable("id") @Min(1) Long id) {
         Wallet walletById = walletService.getWalletById(id);
         return walletMapper.toDto(walletById);
     }
@@ -43,19 +46,19 @@ public class WalletController {
     }
 
     @PutMapping("/{id}")
-    public WalletDto updateWalletById(@PathVariable("id") Long id, @RequestBody WalletDto walletDto) {
+    public WalletDto updateWalletById(@PathVariable("id")@Min(1) Long id, @RequestBody WalletDto walletDto) {
         Wallet wallet = walletMapper.toModel(walletDto);
         Wallet updatedWallet = walletService.updateWallet(wallet, id);
         return walletMapper.toDto(updatedWallet);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteWallet(@PathVariable("id") Long id) {
+    public void deleteWallet(@PathVariable("id") @Min(1) Long id) {
         walletService.deleteWalletById(id);
     }
 
     @PostMapping(path = "/{id}/transactions")
-    public WalletDto operationWallet(@RequestBody TransactionDto transactionDto, @PathVariable Long id, @RequestParam TransactionType type) {
+    public WalletDto operationWallet(@RequestBody TransactionDto transactionDto, @PathVariable("id") @Min(1) Long id, @RequestParam TransactionType type) {
         Transaction transaction = transactionMapper.toModel(transactionDto);
         transaction.setWalletId(id);
         transaction.setType(type);
@@ -63,10 +66,11 @@ public class WalletController {
     }
 
     @GetMapping("/{id}/transactions")
-    public List<TransactionDto> getAllTransactionsByParams(@PathVariable Long id,
+    public List<TransactionDto> getAllTransactionsByParams(@PathVariable("id") @Min(1) Long id,
                                                            @RequestParam(value = "type", required = false) TransactionType type,
                                                            @RequestParam(value = "firstDate", required = false) LocalDate startDate,
                                                            @RequestParam(value = "secondDate", required = false) LocalDate endDate) {
+      walletService.validateWalletExists(id);
       return transactionMapper.toDto(walletService.getAllTransactions(id,type,startDate,endDate));
     }
 }
--- src/main/java/io/github/nuto17/financetracker/repository/WalletRepository.java
+++ src/main/java/io/github/nuto17/financetracker/repository/WalletRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WalletRepository extends JpaRepository<Wallet, Long> {}
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    boolean existsById(Long walletId);
+}
